77da0a87729dca1271ea65a6103f9a434b0097e0
Ishu Kumar Pathak <pishu1766@gmail.com>
2023-11-01 20:40:16 +0530

    basic setup

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,13 @@
+
+export const ErrorPage = () => {
+  return (
+    <div className=" h-screen
+     w-screen flex justify-center
+    items-center">
+        <h1 className="text-2xl font-bold
+        "> oops 404!</h1>
+        <p>Page not found please check your route path</p>
+        
+    </div>
+  )
+}
